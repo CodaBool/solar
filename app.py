ddac79e30be5726259cc081afd9385419ba88124
Coda Bool <codabool@pm.me>
2023-07-16 21:58:44 -0400

    adjust for est

--- app.py
+++ app.py
@@ -15,30 +15,31 @@ print ("solar", int(sense.active_solar_power), "W")
 client = MongoClient(os.environ.get("MONGO_URI"))
 
 now = datetime.now()
+est = now - timedelta(hours = 4) # est is -4
 db = client['myFirstDatabase']
-col = db['days']
+col = db['dd']
 col.create_index('expireAt', expireAfterSeconds=0)
 col.insert_one({
   "usage": int(sense.active_power),
   "solar": int(sense.active_solar_power),
-  "day": now.weekday(),
-  "expireAt": now + timedelta(seconds=604800),
-  'createdAt': now,
+  "day": est.weekday(),
+  "expireAt": est + timedelta(seconds=604800),
+  'createdAt': est,
 })
 
 # only write monthly data at the end of the month
-last_day = calendar.monthrange(now.date().year, now.date().month)[1]
-is_last_day = now.date().day == last_day
-target_time = datetime.combine(now.date(), datetime.min.time()) + timedelta(hours=23, minutes=55)
-if not is_last_day and now.time() < target_time.time():
-  print(f"month data upload in {last_day - now.date().day} days and {target_time-now} hours")
+last_day = calendar.monthrange(est.date().year, est.date().month)[1]
+is_last_day = est.date().day == last_day
+target_time = datetime.combine(est.date(), datetime.min.time()) + timedelta(hours=23, minutes=55)
+if not is_last_day and est.time() < target_time.time():
+  print(f"month data upload in {last_day - est.date().day} days and {target_time-est} hours")
   exit(0)
 
 # Check if a document exists with the given query
 col = db['months']
 document_exists = col.count_documents({
-  'month': now.month,
-  'year': now.year
+  'month': est.month,
+  'year': est.year
 }) > 0
 
 # if there is no montly data for this month, add it
@@ -49,9 +50,9 @@ if not document_exists:
   col.insert_one({
     "average_usage": int(sense.monthly_usage),
     "average_solar": int(sense.monthly_production),
-    "month": now.month,
-    "year": now.year,
-    'createdAt': now,
+    "month": est.month,
+    "year": est.year,
+    'createdAt': est,
   })
 else:
   print("no need to update monthly data")
