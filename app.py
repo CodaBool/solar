91f3ff8c3dcdfd7c1af1a66d12cfb489476f0f89
Coda Bool <codabool@pm.me>
2023-07-16 22:10:51 -0400

    use timezone lib

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from dotenv import load_dotenv
 import os
 from pymongo import MongoClient
 from datetime import datetime, timedelta
+from pytz import timezone
 import calendar
 
 load_dotenv()
@@ -14,32 +15,33 @@ print ("usage", int(sense.active_power), "W")
 print ("solar", int(sense.active_solar_power), "W")
 client = MongoClient(os.environ.get("MONGO_URI"))
 
-now = datetime.now()
-est = now - timedelta(hours = 4) # est is -4
+# set timezone to EST
+now = datetime.now(timezone('US/Eastern'))
 db = client['myFirstDatabase']
 col = db['days']
 col.create_index('expireAt', expireAfterSeconds=0)
+
 col.insert_one({
   "usage": int(sense.active_power),
   "solar": int(sense.active_solar_power),
-  "day": est.weekday(),
-  "expireAt": est + timedelta(seconds=604800),
-  'createdAt': est,
+  "day": now.weekday(),
+  "expireAt": now + timedelta(seconds=604800),
+  'createdAt': now,
 })
 
 # only write monthly data at the end of the month
-last_day = calendar.monthrange(est.date().year, est.date().month)[1]
-is_last_day = est.date().day == last_day
-target_time = datetime.combine(est.date(), datetime.min.time()) + timedelta(hours=23, minutes=55)
-if not is_last_day and est.time() < target_time.time():
-  print(f"month data upload in {last_day - est.date().day} days and {target_time-est} hours")
+last_day = calendar.monthrange(now.date().year, now.date().month)[1]
+is_last_day = now.date().day == last_day
+target_time = datetime.combine(now.date(), datetime.min.time()) + timedelta(hours=23, minutes=55)
+if not is_last_day and now.time() < target_time.time():
+  print(f"month data upload in {last_day - now.date().day} days and {target_time-now} hours")
   exit(0)
 
 # Check if a document exists with the given query
 col = db['months']
 document_exists = col.count_documents({
-  'month': est.month,
-  'year': est.year
+  'month': now.month,
+  'year': now.year
 }) > 0
 
 # if there is no montly data for this month, add it
@@ -50,9 +52,9 @@ if not document_exists:
   col.insert_one({
     "average_usage": int(sense.monthly_usage),
     "average_solar": int(sense.monthly_production),
-    "month": est.month,
-    "year": est.year,
-    'createdAt': est,
+    "month": now.month,
+    "year": now.year,
+    'createdAt': now,
   })
 else:
   print("no need to update monthly data")
